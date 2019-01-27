e03cb3f0b8789b0480a238c8ce7a7edb6a71efdf
Evgenii Koriakin <eakoriakin@yandex.ru>
2019-01-27 10:13:27 +0400

    Export Location type.

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,4 +1,6 @@
 export * from './country.interface';
 export * from './country.type';
+export * from './location.interface';
+export * from './location.type';
 export * from './port.interface';
 export * from './port.type';
