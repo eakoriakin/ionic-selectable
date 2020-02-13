fa5ed88e99a37dbfa9cfcc207a162cab62f3549b
Edy Avendaño <edy-ap@outlook.com>
2020-02-13 13:30:53 -0600

    docs: add no icon example

--- src/app/pages/icon-template/icon-template.ts
+++ src/app/pages/icon-template/icon-template.ts
@@ -14,6 +14,7 @@ import { Port } from '../../types';
 export class IconTemplatePage implements OnInit {
   ports: Port[];
   port: Port;
+  portCustom: Port;
 
   constructor(
     private portService: PortService
