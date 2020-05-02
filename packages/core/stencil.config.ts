62608129a7731b7753c2a3fafcc68c5895d98200
Edy Avendaño <edy-ap@outlook.com>
2020-05-02 01:32:34 -0500

    Feature: Add copy package

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -6,7 +6,10 @@ export const config: Config = {
   outputTargets: [
     {
       type: 'dist',
-      esmLoaderPath: '../loader'
+      esmLoaderPath: '../loader',
+      copy: [
+        { src: '../package.json' }
+      ]
     },
     {
       type: 'docs-readme'
