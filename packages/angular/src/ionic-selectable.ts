345fb8fce20a6bed5fc41674936ed04ce441fba1
Edy Avendaño <edy-ap@outlook.com>
2020-05-02 03:33:33 -0500

    Feature(angular): Add items property

--- packages/angular/src/ionic-selectable.ts
+++ packages/angular/src/ionic-selectable.ts
@@ -1,17 +1,31 @@
-import { EventEmitter, ChangeDetectorRef, ElementRef, NgZone, Component, ChangeDetectionStrategy } from "@angular/core";
+import {
+  EventEmitter,
+  ChangeDetectorRef,
+  ElementRef,
+  NgZone,
+  Component,
+  ChangeDetectionStrategy,
+} from "@angular/core";
 import { proxyOutputs, ProxyCmp } from "./proxies-utils";
 import { Components } from "test-isc";
 
-export declare interface IonicSelectable extends Components.IonicSelectable {
-}
-@ProxyCmp({ inputs: ["value", "items"], "methods": ["open"] })
-@Component({ selector: "ionic-selectable", changeDetection: ChangeDetectionStrategy.OnPush, template: "<ng-content></ng-content>", inputs: ["cancelText", "compareWith", "disabled", "interface", "interfaceOptions", "mode", "multiple", "name", "okText", "placeholder", "selectedText", "value"] })
+export declare interface IonicSelectable extends Components.IonicSelectable {}
+@ProxyCmp({ inputs: ["value", "items"], methods: ["open"] })
+@Component({
+  selector: "ionic-selectable",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: "<ng-content></ng-content>",
+  inputs: [
+    "items",
+    "value",
+  ],
+})
 export class IonicSelectable {
-    onChanged!: EventEmitter<CustomEvent>;
-    protected el: HTMLElement;
-    constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
-        c.detach();
-        this.el = r.nativeElement;
-        proxyOutputs(this, this.el, ["onChanged"]);
-    }
+  onChanged!: EventEmitter<CustomEvent>;
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+    proxyOutputs(this, this.el, ["onChanged"]);
+  }
 }
