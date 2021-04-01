b95094ac6280972d6649869a8bdac1018a29f7fb
Edy Avendaño <edy-ap@outlook.com>
2021-04-01 06:08:52 -0600

    fix(angular): rename component

--- packages/angular/src/index.ts
+++ packages/angular/src/index.ts
@@ -1,5 +1,5 @@
 //COMPONENT
-export { IonicSelectable } from './ionic-selectable/ionic-selectable.component';
+export { IonicSelectableComponent } from './ionic-selectable/ionic-selectable.component';
 
 // PACKAGE MODULE
 export { IonicSelectableModule } from './ionic-selectable/ionic-selectable.module';
